43a21d179820070ae53e68595e5a72d44a739e6c
Xeleonn <tylerjameseaker@gmail.com>
2025-12-01 01:24:44 -0800

    Added image viewing functionality

--- src/gui/ImagePreviewPanel.cpp
+++ src/gui/ImagePreviewPanel.cpp
@@ -10,14 +10,21 @@ ImagePreviewPanel::ImagePreviewPanel()
 void ImagePreviewPanel::InitWidgets()
 {
     main_layout = new QGridLayout();
+    graphics_view = new QGraphicsView();
+    img_scene = new QGraphicsScene(this);
+    img_pixmap = new QPixmap(test_img);
+    img_pixmap_item = new QGraphicsPixmapItem(*img_pixmap);
 }
 
 void ImagePreviewPanel::InitLayouts()
 {
+    graphics_view->setScene(img_scene);
+    img_scene->addItem(img_pixmap_item);
+    main_layout->addWidget(graphics_view);
     this->setLayout(main_layout);
 }
 
 void ImagePreviewPanel::InitWidgetProperties()
 {
-    this->setStyleSheet("background-color: red;");
+    this->setStyleSheet(BACKGROUND_COLOR);
 }
--- src/gui/ImagePreviewPanel.h
+++ src/gui/ImagePreviewPanel.h
@@ -3,6 +3,9 @@
 #include <QWidget>
 #include <QGraphicsView>
 #include <QGridLayout>
+#include <QPixmap>
+#include <QGraphicsPixmapItem>
+#include <qdir.h>
 
 class ImagePreviewPanel : public QWidget
 {
@@ -13,7 +16,15 @@ public:
     ~ImagePreviewPanel() = default;
 
 private:
-    QLayout *main_layout = nullptr;
+    QLayout* main_layout = nullptr;
+    const QString BACKGROUND_COLOR = "background-color: #292929;";
+
+    QGraphicsView* graphics_view = nullptr;
+    QGraphicsScene* img_scene = nullptr;
+    QPixmap* img_pixmap = nullptr;
+    QGraphicsPixmapItem* img_pixmap_item = nullptr;
+
+    QString test_img = "../resources/test_cat.jpg";
 
     void InitWidgets();
     void InitLayouts();
