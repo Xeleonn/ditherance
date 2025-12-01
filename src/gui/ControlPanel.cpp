0d84afc295c08168637b722a402483f1c95bca69
Xeleonn <tylerjameseaker@gmail.com>
2025-11-30 22:42:39 -0800

    Changed magic numbers to private members in ControlPanel class

--- src/gui/ControlPanel.cpp
+++ src/gui/ControlPanel.cpp
@@ -10,23 +10,23 @@ ControlPanel::ControlPanel()
 void ControlPanel::InitWidgets()
 {
     main_layout = new QVBoxLayout();
+    main_layout->setAlignment(ALIGNMENT);
 
     // Title label
     title_label = new QLabel(this);
-    title_label->setText("Control Panel");
-    title_label->setAlignment(Qt::AlignCenter);
-    title_label->setFixedHeight(25);
+    title_label->setText(TITLE_LABEL_NAME);
+    title_label->setAlignment(TITLE_LABEL_ALIGNMENT);
+    title_label->setFixedHeight(TITLE_LABEL_HEIGHT);
 
     // Pixeliness slider
     pixeliness_slider = new QSlider(this);
-    pixeliness_slider->setMinimum(1);
-    pixeliness_slider->setMaximum(20);
-    pixeliness_slider->setOrientation(Qt::Horizontal);
+    pixeliness_slider->setMinimum(PIXELINESS_SLIDER_MIN);
+    pixeliness_slider->setMaximum(PIXELINESS_SLIDER_MAX);
+    pixeliness_slider->setOrientation(PIXELINESS_SLIDER_ORIENTATION);
 }
 
 void ControlPanel::InitLayouts()
 {
-    main_layout->setAlignment(Qt::AlignTop);
     main_layout->addWidget(title_label);
     main_layout->addWidget(pixeliness_slider);
     this->setLayout(main_layout);
@@ -35,10 +35,10 @@ void ControlPanel::InitLayouts()
 void ControlPanel::InitWidgetProperties()
 {
     // Style
-    this->setStyleSheet("color: white;" "background-color: black;");
+    this->setStyleSheet(TEXT_COLOR + BACKGROUND_COLOR);
 
     // Size
     this->setMinimumWidth(WIDTH);
     this->setMaximumWidth(WIDTH);
-    this->setSizePolicy(QSizePolicy::Fixed, QSizePolicy::Expanding);
+    this->setSizePolicy(SIZE_POLICY_W, SIZE_POLICY_H);
 }
--- src/gui/ControlPanel.h
+++ src/gui/ControlPanel.h
@@ -15,11 +15,26 @@ public:
     ~ControlPanel() = default;
 
 private:
-    const int WIDTH = 300;
+    // Main layout
+    QLayout* main_layout = nullptr;
+    const int WIDTH                                     = 300;
+    const Qt::Alignment ALIGNMENT                       = Qt::AlignTop;
+    const QSizePolicy::Policy SIZE_POLICY_H             = QSizePolicy::Expanding;
+    const QSizePolicy::Policy SIZE_POLICY_W             = QSizePolicy::Fixed;
+    const QString TEXT_COLOR                            = "color: white;";
+    const QString BACKGROUND_COLOR                      = "background-color: #292929;";
 
-    QLayout *main_layout = nullptr;
+    // Title label
     QLabel *title_label = nullptr;
+    const QString TITLE_LABEL_NAME                      = "Control Panel";
+    const Qt::Alignment TITLE_LABEL_ALIGNMENT           = Qt::AlignCenter;
+    const int TITLE_LABEL_HEIGHT                        = 25;
+
+    // Pixeliness slider
     QAbstractSlider *pixeliness_slider = nullptr;
+    const int PIXELINESS_SLIDER_MIN                     = 1;
+    const int PIXELINESS_SLIDER_MAX                     = 20;
+    const Qt::Orientation PIXELINESS_SLIDER_ORIENTATION = Qt::Horizontal;
 
     void InitWidgets();
     void InitLayouts();
